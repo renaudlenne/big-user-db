dd9237502c9131cb73a95333b64ae726fcbd583c
Renaud Lenne <renaud@26lights.com>
2020-03-02 15:20:23 +0100

    Added resolvers for getters

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,8 +1,34 @@
+import faker from "faker/locale/en";
+const Hashids = require('hashids/cjs');
+
+const hashids = new Hashids();
+const initialSeed = 26;
+const initialPageSize = 10;
+
 export default {
   Query: {
     users() {
+      //TODO pagination
+      return [...Array(initialPageSize)].map((_, i) => {
+        faker.seed(initialSeed+i);
+
+        const firstName = faker.name.firstName(),
+          lastName = faker.name.lastName();
+
+        return {
+          id: `u.${hashids.encode(i)}`,
+          email: faker.internet.email(firstName, lastName),
+          firstName,
+          lastName,
+          phone: faker.phone.phoneNumber(),
+          avatar: faker.image.avatar(),
+          bitcoinAddress: faker.finance.bitcoinAddress(),
+          birthDate: faker.date.past(50, new Date(2020, 2, 1)),
+        };
+      });
     },
   },
+  //TODO updaters
   Mutation: {
     async updateAddress(object, { addressInput }) {
     },
@@ -15,19 +41,101 @@ export default {
   },
   User: {
     async jobs(object) {
+      const userId = object.id;
+      if (!userId || !userId.startsWith('u.')) {
+        throw new Error('Incorrect user')
+      }
+      const numericalId = hashids.decode(userId.substring(2))[0];
+      faker.seed(numericalId);
+      return [...Array(Math.floor(faker.random.number({min: 0, max: 2})))].map((_, i) => ({
+        id: `j.${hashids.encode(numericalId*10+i)}`,
+        title: faker.name.jobTitle(),
+        descriptor: faker.name.jobDescriptor(),
+        type: faker.name.jobType(),
+        area: faker.name.jobArea(),
+        phone: faker.phone.phoneNumber(),
+        email: faker.internet.email(object.firstName, object.lastName, faker.internet.domainName()),
+        startDate: faker.date.past(3, new Date(2020, 2, 1)),
+      }));
     },
     async address(object) {
+      const userId = object.id;
+      if (!userId || !userId.startsWith('u.')) {
+        throw new Error('Incorrect user')
+      }
+      const numericalId = hashids.decode(userId.substring(2))[0];
+      faker.seed(numericalId);
 
+      return {
+        id: `a.${hashids.encode(numericalId*10)}`,
+        streetAddress: faker.address.streetAddress(),
+        secondaryAddress: faker.address.secondaryAddress(),
+        zipCode: faker.address.zipCode(),
+        city: faker.address.city(),
+        country: faker.address.country(),
+        latitude: faker.address.latitude(),
+        longitude: faker.address.longitude(),
+      }
     },
   },
   JobPosition: {
     async company(object) {
+      const jobId = object.id;
+      if (!jobId || !jobId.startsWith('j.')) {
+        throw new Error('Incorrect job')
+      }
+      const numericalId = hashids.decode(jobId.substring(2))[0];
+      faker.seed(numericalId);
+
+      return {
+        id: `c.${hashids.encode(numericalId*10)}`,
+        name: faker.company.companyName(0),
+        description: faker.company.bs(),
+        phone: faker.phone.phoneNumber(),
+      }
     },
   },
   Company: {
     async address(object) {
+      const companyId = object.id;
+      if (!companyId || !companyId.startsWith('c.')) {
+        throw new Error('Incorrect company')
+      }
+      const numericalId = hashids.decode(companyId.substring(2))[0];
+      faker.seed(numericalId);
+
+      return {
+        id: `a.${hashids.encode(numericalId*10)}`,
+        streetAddress: faker.address.streetAddress(),
+        secondaryAddress: faker.address.secondaryAddress(),
+        zipCode: faker.address.zipCode(),
+        city: faker.address.city(),
+        country: faker.address.country(),
+        latitude: faker.address.latitude(),
+        longitude: faker.address.longitude(),
+      }
     },
     async contact(object) {
+      const companyId = object.id;
+      if (!companyId || !companyId.startsWith('c.')) {
+        throw new Error('Incorrect company')
+      }
+      const numericalId = hashids.decode(companyId.substring(2))[0];
+      faker.seed(numericalId);
+
+      const firstName = faker.name.firstName(),
+        lastName = faker.name.lastName();
+
+      return {
+        id: `u.${hashids.encode(numericalId*10)}`,
+        email: faker.internet.email(firstName, lastName),
+        firstName,
+        lastName,
+        phone: faker.phone.phoneNumber(),
+        avatar: faker.image.avatar(),
+        bitcoinAddress: faker.finance.bitcoinAddress(),
+        birthDate: faker.date.past(50, new Date(2020, 2, 1)),
+      };
     },
   }
 }
--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,8 +1,17 @@
 import { makeExecutableSchema } from 'apollo-server-express';
+import { GraphQLDate } from 'graphql-iso-date';
+import merge from 'lodash/merge';
 
 import typeDefs from './schema.graphql';
 import resolvers from './resolvers';
 
-const schema = makeExecutableSchema({ typeDefs, resolvers, });
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers: merge(
+    {
+      Date: GraphQLDate,
+    },
+    resolvers),
+});
 
 export default schema;
